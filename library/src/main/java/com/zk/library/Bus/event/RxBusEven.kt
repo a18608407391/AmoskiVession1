45d3de0f4622ac999e548fb2f951b9acc77d09c0
a18608407391 <zuolin19900325>
2020-02-27 00:09:16 +0800

    fixup! 啛啛喳喳213123

---
+++ library/src/main/java/com/zk/library/Bus/event/RxBusEven.kt
@@ -0,0 +1,22 @@
+package com.zk.library.Bus.event
+
+class RxBusEven {
+
+    companion object {
+        var DriverReturnRequest =  0x1001
+        var PartyWebViewReturn = 0x1002
+        var DriverMapPointRegeocodeSearched = 0x1003
+        var MapCameraChangeFinish = 0x1004
+    }
+
+
+    var type = 0
+
+    var value: Any? = null
+
+    var secondValue :Any ? = null
+
+    var parameter: HashMap<Any, Any>? = null
+
+
+}
